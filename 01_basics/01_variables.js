3d2ffc7b8df5f7c22038a1cae5d1ac7c057e385d
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-10 10:03:21 +0530

    created 01_basics folder which contains 01_variables file of js-varibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453;
+let accountEmail = "akhilworkplace@gmail.com";
+var accountPassword = "12345";
+let accountState;
+
+accountCity = "Jaipur";
+accountCity = "Delhi";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/

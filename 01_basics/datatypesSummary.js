33ba158363fadb7d094b1929a2b5e5b63fec38f9
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-10 17:39:40 +0530

    noted different data types in Javascript with examples in datatypesSummary.js

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,34 @@
+//Primitive data types
+    // string, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const isOusideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+const bigNumber = 446546678465467461454n; 
+// console.log(typeof bigNumber)   
+//Reference data types (Non primitive)
+
+    // Array, Objects, function
+    // []    , {}    , ()
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+
+const obj = {
+    name: "akhil", 
+    age: 23, 
+    isSpeak: true
+}
+
+let sum = function(n1, n2){
+    return n1 + n2;
+}
+

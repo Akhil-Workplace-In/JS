28a17a9f243fdb62d9f7bf6663e13f55004faeb1
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-10 11:28:35 +0530

    noted conversion of data types in 03_conversionOperatins.js file

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = 33;
+
+let score1 = null;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// console.log("valueInNumber", valueInNumber);
+// console.log("valueInNumber1", valueInNumber1);
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log("booleanIsLoggedIn", booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "akhil" => true
+
+//string
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log("stringNumber", stringNumber);
+console.log(typeof stringNumber);

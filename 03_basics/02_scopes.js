763b136e9d241761e940238221c2fee2a3ed6c69
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 18:55:38 +0530

    concept on scopes in js {} in 02_scopes.js file

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+
+
+// function one(){
+//     const username = "akhil"
+
+//     function two(){
+//         const website = "instagram"
+//         console.log(username);
+//     }
+
+//     // console.log(website);
+//     two()
+// }
+
+// one();
+
+if(true){
+    const username = "akhil";
+    if(username === "akhil"){
+         const website = "instagram";
+        //  console.log(username+ website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// **************** interesting **************
+
+console.log(addOne(5));
+function  addOne(num){
+    return num + 1;
+}
+
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2;
+}
+

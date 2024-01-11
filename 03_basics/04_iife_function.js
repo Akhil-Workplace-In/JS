8789403acc4addbc6334cebd705b954ada82071b
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 21:08:40 +0530

    noted comment related to IIFE in 04_iife_function.js file

--- 03_basics/04_iife_function.js
+++ 03_basics/04_iife_function.js
@@ -3,8 +3,9 @@
 (function chai(){
     //named IIFE
     console.log("DB CONNECTED");
-})();
+})(); //don't forget to put ;(terminator) at the end of iife to run other following iife
 
+//parameter passed in iffe
 ((name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 })('akhil');

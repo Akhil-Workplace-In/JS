8a448adde5393ecee62457bb529bd5e321be2fe9
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 15:57:45 +0530

    noted some more concept of objects in 04_objects.js file

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,61 @@
+// const tinderApp = new Object();
+
+const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sam";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com", 
+    fullName: {
+        usersFullname: {
+            firstName: "akhil",
+            lastName: "yadav"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.usersFullname["firstName"]);
+const obj1 = {
+    1: "a",
+    2: "b",
+    3: "c", 
+    4: "d"
+}
+
+const obj2 = {
+    5: "e", 
+    6: "f",
+    7: "g", 
+    8: "h"
+}
+
+// const obj3 = {obj1, obj2}
+
+const obj3 = Object.assign(obj1, obj2)
+
+const obj4 = Object.assign({}, obj1, obj2); // Object.assign
+
+const obj5 = {...obj1, ...obj2}
+// console.log(obj4);
+// console.log(obj5);
+
+const users = [
+    {
+        id: 1, 
+        email: "akhil@gmail.com"
+    }, 
+    {
+
+    }
+];
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));

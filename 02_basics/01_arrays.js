cec3ffb1f4a417ed80603cbd4f7d667f5bf14929
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 13:15:20 +0530

    noted some more array methods in 02_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeroes = ["shaktiman", "naagran", "hatim"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+// console.log(myArr2[0]);
+
+//Array methods
+
+// myArr.push(3);
+// myArr.pop()
+
+// myArr.unshift(56);
+// myArr.shift()
+
+// console.log(myArr.includes(45));
+// console.log(myArr.indexOf(8));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr)
+
+// slice , splice 
+
+// console.log("A", myArr)
+// const myn1 = myArr.slice(1, 3);
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(0, 3);
+// console.log(myn2)
+// console.log("C", myArr)

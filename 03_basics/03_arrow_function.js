bde4b92bb8927012dc9006e4ed8ee5778871fb63
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 20:49:14 +0530

    concept of arrow function (implicits return and explicit return in 03_arrow_function.js file

--- 03_basics/03_arrow_function.js
+++ 03_basics/03_arrow_function.js
@@ -14,6 +14,43 @@ const user = {
 
 // user.welcomeMessage();
 
-user.welcomeMessage()
-console.log(this); // In browser context this refers to browser window object
+// user.welcomeMessage()
+// console.log(this); // In browser context this refers to browser window object
+                    // window is global object in browser
+// function chai(){
+//     const username = "akhil";
+//     console.log(this.username);
+// }
+
+// chai();
+
+//Arrow function
+
+// const chai = function () {
+//     console.log("hi there!");
+// }
+
+// chai()
+
+// const chai = () => {
+//     console.log("hi there");
+//     console.log(this);
+// }
+
+// chai();
+
+const addTwo = (num1, num2) => {
+    return num1 + num2;  
+}
+// implicit return 
+
+// const addTwoNum = (num1, num2) => num1+ num2;
+// const addTwoNum = (num1, num2) => (num1+ num2);
+const addTwoNum = (num1, num2) => ({username: "akhil"});
+
+// console.log(addTwo(45, 45));
+// console.log(addTwoNum(50, 50));
+
+
+
 

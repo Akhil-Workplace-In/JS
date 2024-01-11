3944edb4e946728856a3ed30acb75dd250a6d0da
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-11 22:45:48 +0530

    concept of control flow in js noted in if_else.js file

---
+++ 04_controlFlow/if_else.js
@@ -0,0 +1,34 @@
+//if
+
+if(condition){   //condition (if true then internal code will execute)
+    //scope
+}
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+
+    case 3:
+        console.log("March");
+        break;        
+
+    default:
+        console.log("default");
+        break;
+}
+

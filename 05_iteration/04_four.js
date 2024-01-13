a2d7cdd272bc51cf822aa8ef76f9c91590f40285
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-13 09:54:01 +0530

    concept of for in js noted in 04_four.js file

---
+++ 05_iteration/04_four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: 'javascript',
+    cpp: "c++",
+    rb: "ruby", 
+    swift: "swift by apple"
+}
+
+//for in
+for (const key in myObject) {
+    // console.log(key, myObject[key]);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const lang in programming){
+    // console.log(programming[lang]);
+}

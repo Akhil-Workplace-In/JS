b9022afb538e7a128c6d7479d1d8acecad9ece7a
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-14 17:21:34 +0530

    js reduce method

---
+++ 05_iteration/07_reduce.js
@@ -0,0 +1,11 @@
+const myNums = [1, 2, 3];
+
+// const myTotal = myNums.reduce(function (acc, currVal){
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
+//     return acc + currVal
+// }, 0);
+
+// const myTotal = myNums.reduce((acc, currVal) => (acc + currVal), 0)
+// console.log(myTotal);
+const myTotal = myNums.reduce((acc, currVal) => (acc + currVal), 0);
+console.log(myTotal)

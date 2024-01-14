cdc46f69c4da9a7269b1e0e39a3532b25630b057
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-14 09:47:59 +0530

    Some array methods noted in 05_array_forEach.js file

--- 05_iteration/05_array_forEach.js
+++ 05_iteration/05_array_forEach.js
@@ -31,5 +31,37 @@ const myCoding = [
 ]
 
 myCoding.forEach((item) => {
-    console.log(item.languageName);
+    // console.log(item.languageName);
 } )
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item;
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 5;
+// });
+// console.log(newNums)
+
+// const newNums = [];
+// myNums.forEach((num) => {
+//     if(num > 5){
+//         newNums.push(num);
+//     }
+// })
+// console.log(newNums);
+
+const books = [
+    {title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004}, 
+    {title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008}
+]
+// const userBooks = books.filter((book) => book.genre === "Fiction");
+// console.log(userBooks);
+
+const userBooks = books.filter((book) => {return book.publish > 1990} );
+console.log(userBooks);

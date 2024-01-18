25f14259162270c0fcf5fe6bc3fde574fd2225fa
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-01-18 17:54:25 +0530

    Project Keysrokes detector solved

---
+++ 07_projects/05_keybord_key/chaiaurcode.js
@@ -0,0 +1,22 @@
+console.log('Project 5');
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown', function (e) {
+  insert.innerHTML = `
+    <div class="color">
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Key Code</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === ' ' ? 'space' : e.key}</td>
+          <td>${e.keyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+  `;
+  console.log(e.key);
+});
